8f306e8939b34a5e54d74c19d58b4bb4bd47f176
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-29 15:07:39 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
 # your code goes here
+def begin_with_r(arr)
+  i = 0
+  while i < arr.length
+    arr2 = arr.split('')
+    if arr2.first == 'r'
+      return true
+    else
+      return false
+    end
+    i += 1
+  end
+end
