bd5bf9cbc3e83090278ed120743f5ff9ba72998c
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-29 15:13:52 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,10 +1,15 @@
 # your code goes here
 def begin_with_r(arr)
+  rArray = []
   for i in arr
-    if i.startsWith("r")
-      true
-    else
-      false
+    d = arr[i].split('')
+    if d.first == "r"
+      rArray << d
     end
+  end
+  if rArray.length == arr.length
+    return true
+  else
+    return false
   end
 end
