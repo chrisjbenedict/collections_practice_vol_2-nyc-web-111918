bb0f414f65d524f784ab637a37597f5ec3c06c0b
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-29 15:20:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,16 @@
 # your code goes here
 def begin_with_r(arr)
-
+  arr2 = []
+  arr.each do |item|
+    if item[0] == "r"
+      arr2.push(item)
+    end
+    if arr2.length == arr.length
+      return true
+    end
+    return false
 end
 
 def contain_a(arr)
-  for i in arr
-    d = arr[i].split('')
-    if d.include?('a')
-      return arr[i]
-    end
-  end
+  
 end
